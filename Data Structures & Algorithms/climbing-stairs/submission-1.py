eb2764c60f224b2e0de7301addd07b64bd47f6c6
Sriyansh Gupta <116352112+Shre202002@users.noreply.github.com>
2026-04-06 19:39:20 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def ways(n):
+            if n == 1: return 1
+            if n == 2 : return 2
+            return ways(n-1) + ways(n-2) 
+        return ways(n)
+        
